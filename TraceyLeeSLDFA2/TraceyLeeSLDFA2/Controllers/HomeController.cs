4fc0580d8e18bd3bb511ece47c86d61c6e806b79
Lunar7111 <tswartz03@outlook.com>
2023-08-08 13:14:24 +0200

    Add CI-CD pipeline

---
+++ TraceyLeeSLDFA2/TraceyLeeSLDFA2/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TraceyLeeSLDFA2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult LatestNews()
+        {
+            ViewBag.Message = "Your Latest News page.";
+
+            return Redirect("https://screenrant.com/comics/");
+        }
+
+        public ActionResult Location()
+        {
+          
+
+            return View();
+        }
+
+        public ActionResult Favourites()
+        {
+            ViewBag.Message = "YourFavourites page.";
+
+            return View();
+        }
+    }
+}
